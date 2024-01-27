1daa866261505c84ab111e842c85d9b65d288984
Louis-Philippe St-Arnaud <99094186+louis-p-sta@users.noreply.github.com>
2024-01-27 15:13:22 -0500

    Added camera demo from website.

--- WebApp/PaperIDE/camera/views.py
+++ WebApp/PaperIDE/camera/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index. This will become a camera eventually.")
+
+def Home(request):
+    return render(request, "")
