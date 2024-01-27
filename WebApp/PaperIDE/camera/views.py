79ed208165312b4a8e384e27393f480bcfe541f2
Louis-Philippe St-Arnaud <99094186+louis-p-sta@users.noreply.github.com>
2024-01-27 17:43:47 -0500

    Combined with Django

--- WebApp/PaperIDE/camera/views.py
+++ WebApp/PaperIDE/camera/views.py
@@ -2,8 +2,15 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
+from django.template import loader
+
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index. This will become a camera eventually.")
+    template = loader.get_template('camera.html')
+    return HttpResponse(template.render())
 
 def Home(request):
     return render(request, "")
+
+# def camera(request):
+#     template = loader.get_template('camera.html')
+#     return HttpResponse(template.render())
